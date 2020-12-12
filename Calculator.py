7d7d3a5e184939c588c98b29f51388279c1c3c40
michael <michaelkeaveny0@gmail.com>
2020-12-12 17:03:08 +0000

    Added number display

--- Calculator.py
+++ Calculator.py
@@ -11,6 +11,13 @@ class CalculatorWindow(Gtk.Window):
         grid = Gtk.Grid()
         self.add(grid)
 
+        # Create the widget to display the stored value
+        displayFrame = Gtk.Frame()
+        displayBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        displayLabel = Gtk.Label(label="")
+        displayBox.pack_start(displayLabel, True, True, 0)
+        displayFrame.add(displayBox)
+
         # Create all the number buttons
         numButton1 = Gtk.Button.new_with_label("7")
         numButton2 = Gtk.Button.new_with_label("8")
@@ -23,11 +30,13 @@ class CalculatorWindow(Gtk.Window):
         numButton9 = Gtk.Button.new_with_label("3")
         numButton10 = Gtk.Button.new_with_label("0")
 
+        # Add the number display to the grid, set to column and row 0. Width of 3 and height of 1
+        grid.attach(displayFrame, 0, 0, 3, 1)
         # Add the buttons to the grid
-        grid.add(numButton1)
+        grid.attach_next_to(numButton1, displayFrame, Gtk.PositionType.BOTTOM, 1, 1)
         # Arguments child, sibling, side, width, height
         grid.attach_next_to(numButton2, numButton1, Gtk.PositionType.RIGHT, 1, 1)
-        grid.attach_next_to(numButton3, numButton2, Gtk.PositionType.RIGHT, 1, 1)
+        grid.attach_next_to(numButton3, numButton2, Gtk.PositionType.RIGHT, 1, 1)   
         # Sets the buttons to be positioned directly below their number's value + 3
         grid.attach_next_to(numButton4, numButton1, Gtk.PositionType.BOTTOM, 1, 1)
         grid.attach_next_to(numButton5, numButton2, Gtk.PositionType.BOTTOM, 1, 1)
