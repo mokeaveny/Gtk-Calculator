746cdf1138a19ef533c5ad550faef741c2aa2271
michael <michaelkeaveny0@gmail.com>
2020-12-13 18:20:03 +0000

    Working calculator for two whole integer numbers - need to implement float operations

--- Calculator.py
+++ Calculator.py
@@ -11,6 +11,12 @@ class CalculatorWindow(Gtk.Window):
         grid = Gtk.Grid()
         self.add(grid)
 
+        # Assign attributes to the window to perform operations
+        self.firstNumber = ""
+        self.operator = ""
+        self.secondNumber = ""
+        self.switchNum = False
+
         # Create the widget to display the stored value
         displayFrame = Gtk.Frame()
         displayBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
@@ -43,11 +49,17 @@ class CalculatorWindow(Gtk.Window):
 
         # Create all the operation buttons
         operationButton1 = Gtk.Button.new_with_label("+")
+        operationButton1.connect("clicked", self.addClicked)
         operationButton2 = Gtk.Button.new_with_label("-")
+        operationButton2.connect("clicked", self.subtractClicked)
         operationButton3 = Gtk.Button.new_with_label(chr(247))
+        operationButton3.connect("clicked", self.divideClicked)
         operationButton4 = Gtk.Button.new_with_label("*")
+        operationButton4.connect("clicked", self.multiplyClicked)
         operationButton5 = Gtk.Button.new_with_label("=")
+        operationButton5.connect("clicked", self.calculateClicked)
         operationButton6 = Gtk.Button.new_with_label("CLEAR")
+        operationButton6.connect("clicked", self.clearClicked)
 
         # Add the number display to the grid, set to column and row 0. Width of 3 and height of 1
         grid.attach(displayFrame, 0, 0, 3, 1)
@@ -80,11 +92,29 @@ class CalculatorWindow(Gtk.Window):
         print(currentText)
         self.displayLabel.set_text(currentText + "7")
 
+        # Updates the attribute firstNumber and secondNumber depending on whether an operator has been clicked
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("7")
+                
+            self.secondNumber = self.displayLabel.get_text()
+
     def number8Clicked(self, numButton2):
         print(8)
         currentText = self.displayLabel.get_text()
         print(currentText)
         self.displayLabel.set_text(currentText + "8")
+
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("8")
+            self.secondNumber = self.displayLabel.get_text()
 
     def number9Clicked(self, numButton3):
         print(9)
@@ -92,11 +122,27 @@ class CalculatorWindow(Gtk.Window):
         print(currentText)
         self.displayLabel.set_text(currentText + "9")
 
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("9")
+            self.secondNumber = self.displayLabel.get_text()
+
     def number4Clicked(self, numButton4):
         print(4)
         currentText = self.displayLabel.get_text()
         print(currentText)
         self.displayLabel.set_text(currentText + "4")
+
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("4")
+            self.secondNumber = self.displayLabel.get_text()
 
     def number5Clicked(self, numButton5):
         print(5)
@@ -104,11 +150,27 @@ class CalculatorWindow(Gtk.Window):
         print(currentText)
         self.displayLabel.set_text(currentText + "5")
 
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("5")
+            self.secondNumber = self.displayLabel.get_text()
+
     def number6Clicked(self, numButton6):
         print(6)
         currentText = self.displayLabel.get_text()
         print(currentText)
         self.displayLabel.set_text(currentText + "6")
+
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("6")
+            self.secondNumber = self.displayLabel.get_text()
 
     def number1Clicked(self, numButton7):
         print(1)
@@ -116,11 +178,27 @@ class CalculatorWindow(Gtk.Window):
         print(currentText)
         self.displayLabel.set_text(currentText + "1")
 
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("1")
+            self.secondNumber = self.displayLabel.get_text()
+
     def number2Clicked(self, numButton8):
         print(2)
         currentText = self.displayLabel.get_text()
         print(currentText)
         self.displayLabel.set_text(currentText + "2")
+
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("2")
+            self.secondNumber = self.displayLabel.get_text()
 
     def number3Clicked(self, numButton9):
         print(3)
@@ -128,12 +206,113 @@ class CalculatorWindow(Gtk.Window):
         print(currentText)
         self.displayLabel.set_text(currentText + "3")
 
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("3")
+            self.secondNumber = self.displayLabel.get_text()
+
     def number0Clicked(self, numButton10):
         print(0)
         currentText = self.displayLabel.get_text()
         print(currentText)
         if (currentText != ""):
             self.displayLabel.set_text(currentText + "0")
+
+        if (self.switchNum == False):
+            self.firstNumber = self.displayLabel.get_text()
+        else:
+            # No number has been clicked after the operator has been clicked so display the number that has just been clicked
+            if (self.secondNumber == ""):
+                self.displayLabel.set_text("0")
+            self.secondNumber = self.displayLabel.get_text()
+
+    # If no number has been selected yet then don't assing the operator
+    # Once a number has been selected then switch to appending to the second number
+    def addClicked(self, operationButton1):
+        if (self.firstNumber == ""):
+            print("No number has been selected yet!")
+        else:
+            if (self.operator == ""):
+                self.operator = "+"
+                self.displayLabel.set_text("+")
+                self.switchNum = True
+
+    def subtractClicked(self, operationButton2):
+        if (self.firstNumber == ""):
+            print("No number has been selected yet!")
+        else:
+            if (self.operator == ""):
+                self.operator = "-"
+                self.displayLabel.set_text("-")
+                self.switchNum = True
+
+    def divideClicked(self, operationButton3):
+        if (self.firstNumber == ""):
+            print("No number has been selected yet!")
+        else:
+            if (self.operator == ""):
+                self.operator = "/"
+                self.displayLabel.set_text(chr(247))
+                self.switchNum = True
+
+    def multiplyClicked(self, operationButton4):
+        if (self.firstNumber == ""):
+            print("No number has been selected yet!")
+        else:
+            if (self.operator == ""):
+                self.operator = "*"
+                self.displayLabel.set_text("*")
+                self.switchNum = True
+
+    def calculateClicked(self, operationButton5):
+        if (self.firstNumber == ""):
+            print("No first number has been selected")
+        elif (self.secondNumber == ""):
+            print("No second number has been selected")
+        else:
+            if (self.operator == "+"):
+                result = int(self.firstNumber) + int(self.secondNumber)
+                self.firstNumber = str(result)
+                self.displayLabel.set_text(self.firstNumber)
+                # Once the calculation has been performed and output then set the operator and secondNumber to blank
+                self.operator = ""
+                self.secondNumber = ""
+
+            elif (self.operator == "-"):
+                result = int(self.firstNumber) - int(self.secondNumber)
+                self.firstNumber = str(result)
+                self.displayLabel.set_text(self.firstNumber)
+                self.operator = ""
+                self.secondNumber = ""
+
+            elif (self.operator == "/"):
+                result = int(self.firstNumber) / int(self.secondNumber)
+                self.firstNumber = str(result)
+                self.displayLabel.set_text(self.firstNumber)
+                self.operator = ""
+                self.secondNumber = ""
+
+            elif (self.operator == "*"):
+                result = int(self.firstNumber) * int(self.secondNumber)
+                self.firstNumber = str(result)
+                self.displayLabel.set_text(self.firstNumber)
+                self.operator = ""
+                self.secondNumber = ""
+
+
+    def clearClicked(self, operationButton6):
+        print("CLEAR")
+        # Clears all of the stored numbers and the operator
+        self.firstNumber = ""
+        self.operator = ""
+        self.secondNumber = ""
+        self.switchNum = False
+        # Sets the display to empty
+        self.displayLabel.set_text("")
+        print(self.operator)
 
 win = CalculatorWindow()
 win.connect("destroy", Gtk.main_quit)
